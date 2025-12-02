ca7e7812128dd8e87a1cf68b94afd8d80786c551
llatzhar <llatzhar@gmail.com>
2025-12-02 11:29:15 +0900

    visual Studio2026対応

--- EyeMuscle/App.xaml.cs
+++ EyeMuscle/App.xaml.cs
@@ -1,61 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace EyeMuscle
 {
     /// <summary>
-    /// 既定の Application クラスを補完するアプリケーション固有の動作を提供します。
+    /// Provides application-specific behavior to supplement the default <see cref="Application"/> class.
     /// </summary>
-    sealed partial class App : Application
+    public sealed partial class App : Application
     {
         /// <summary>
-        /// 単一アプリケーション オブジェクトを初期化します。これは、実行される作成したコードの
-        ///最初の行であるため、main() または WinMain() と論理的に等価です。
+        /// Initializes the singleton application object. This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            InitializeComponent();
+
+            Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// アプリケーションがエンド ユーザーによって正常に起動されたときに呼び出されます。他のエントリ ポイントは、
-        /// アプリケーションが特定のファイルを開くために起動されたときなどに使用されます。
-        /// </summary>
-        /// <param name="e">起動の要求とプロセスの詳細を表示します。</param>
+        /// <inheritdoc/>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // ウィンドウに既にコンテンツが表示されている場合は、アプリケーションの初期化を繰り返さずに、
-            // ウィンドウがアクティブであることだけを確認してください
-            if (rootFrame == null)
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active.
+            if (Window.Current.Content is not Frame rootFrame)
             {
-                // ナビゲーション コンテキストとして動作するフレームを作成し、最初のページに移動します
+                // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
-
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    //TODO: 以前中断したアプリケーションから状態を読み込みます
+                    // TODO: Load state from previously suspended application
                 }
 
-                // フレームを現在のウィンドウに配置します
+                // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
@@ -63,37 +47,38 @@ namespace EyeMuscle
             {
                 if (rootFrame.Content == null)
                 {
-                    // ナビゲーション スタックが復元されない場合は、最初のページに移動します。
-                    // このとき、必要な情報をナビゲーション パラメーターとして渡して、新しいページを
-                    //構成します
+                    // When the navigation stack isn't restored navigate to the first page, configuring
+                    // the new page by passing required information as a navigation parameter.
                     rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
-                // 現在のウィンドウがアクティブであることを確認します
+
+                // Ensure the current window is active
                 Window.Current.Activate();
             }
         }
 
         /// <summary>
-        /// 特定のページへの移動が失敗したときに呼び出されます
+        /// Invoked when Navigation to a certain page fails.
         /// </summary>
-        /// <param name="sender">移動に失敗したフレーム</param>
-        /// <param name="e">ナビゲーション エラーの詳細</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        /// <param name="sender">The Frame which failed navigation.</param>
+        /// <param name="e">Details about the navigation failure.</param>
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+            throw new Exception($"Failed to load page '{e.SourcePageType.FullName}'.");
         }
 
         /// <summary>
-        /// アプリケーションの実行が中断されたときに呼び出されます。
-        /// アプリケーションが終了されるか、メモリの内容がそのままで再開されるかに
-        /// かかわらず、アプリケーションの状態が保存されます。
+        /// Invoked when application execution is being suspended. Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
         /// </summary>
-        /// <param name="sender">中断要求の送信元。</param>
-        /// <param name="e">中断要求の詳細。</param>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: アプリケーションの状態を保存してバックグラウンドの動作があれば停止します
+            SuspendingDeferral deferral = e.SuspendingOperation.GetDeferral();
+
+            // TODO: Save application state and stop any background activity
             deferral.Complete();
         }
     }
